454695c725bb6196723276703716e81f1928b1f7
Rohan Bhatia <rohanb459@gmail.com>
2022-04-24 15:59:31 +0530

    request and link extracted for results

--- main.js
+++ main.js
@@ -1,3 +1,26 @@
 const url = ("https://www.espncricinfo.com/series/indian-premier-league-2022-1298423");
 let request = require("request");
 let cheerio = require("cheerio");
+
+request(url, cb);
+
+function cb(err, response, html)
+{
+    if(err)
+    console.log(err);
+    else
+    {
+        extractLink(html);
+    }
+    // console.log(html);
+}
+
+function extractLink(html)
+{
+    let $ = cheerio.load(html);
+    let anchorElem = $(".ds-block.ds-text-center");
+    let link = anchorElem.attr("href");
+    
+    let fullLink = "https://www.espncricinfo.com"+link;
+    console.log(fullLink);
+}
