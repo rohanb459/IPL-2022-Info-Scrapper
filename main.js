bf55ab43ed7f64a6cf208db1a75c4f4d24995d29
Rohan Bhatia <rohanb459@gmail.com>
2022-04-25 16:18:26 +0530

    request added

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+// The project is not completed yet waiting for IPL to finish down.
+
 const url = ("https://www.espncricinfo.com/series/indian-premier-league-2022-1298423");
 let request = require("request");
 let cheerio = require("cheerio");
@@ -19,8 +21,38 @@ function extractLink(html)
 {
     let $ = cheerio.load(html);
     let anchorElem = $(".ds-block.ds-text-center");
+    console.log(anchorElem);
     let link = anchorElem.attr("href");
     
-    let fullLink = "https://www.espncricinfo.com"+link;
+    let fullLink = "https://www.espncricinfo.com/series/indian-premier-league-2022-1298423/match-results";
+    console.log("--------------------------------");
     console.log(fullLink);
+    getAllMatchesLink(fullLink);
+}
+function getAllMatchesLink(url)
+{ 
+    request(url, function (err, response, html){
+        if(err)
+        console.log(err);
+        else
+        extractMatchLink(html);
+    })
+}
+
+function extractMatchLink(html)
+{
+    let $ = cheerio.load(html);
+    let cardLinksArr = $("div.ds-px-4.ds-py-3>a");
+    // console.log(cardLinksArr);
+    // console.log(cardLinksArr.length);
+    // let scoreCardLinks=[];
+    // for(let i=0; i<cardLinksArr.length; i++)
+    // {
+    //     let tempLink = cardLinksArr[i].attribs["href"];
+    //     if(tempLink.includes("full-scorecard"))
+    //     console.log(tempLink);
+    // }
+    // console.log("`````````````````````````````````");
+    // console.log(scoreCardLinks);
+    
 }
