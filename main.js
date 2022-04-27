8b4f0d3591903b68909f8203d9e737af73173dc5
Rohan Bhatia <rohanb459@gmail.com>
2022-04-27 15:08:41 +0530

    match links extracted

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 // The project is not completed yet waiting for IPL to finish down.
 
-const url = ("https://www.espncricinfo.com/series/indian-premier-league-2022-1298423");
+const url = ("https://www.espncricinfo.com/series/indian-premier-league-2022-1298423/match-results");
 let request = require("request");
 let cheerio = require("cheerio");
 
@@ -12,47 +12,22 @@ function cb(err, response, html)
     console.log(err);
     else
     {
-        extractLink(html);
+        extractMatchLink(html);
     }
     // console.log(html);
-}
-
-function extractLink(html)
-{
-    let $ = cheerio.load(html);
-    let anchorElem = $(".ds-block.ds-text-center");
-    console.log(anchorElem);
-    let link = anchorElem.attr("href");
-    
-    let fullLink = "https://www.espncricinfo.com/series/indian-premier-league-2022-1298423/match-results";
-    console.log("--------------------------------");
-    console.log(fullLink);
-    getAllMatchesLink(fullLink);
-}
-function getAllMatchesLink(url)
-{ 
-    request(url, function (err, response, html){
-        if(err)
-        console.log(err);
-        else
-        extractMatchLink(html);
-    })
 }
 
 function extractMatchLink(html)
 {
     let $ = cheerio.load(html);
-    let cardLinksArr = $("div.ds-px-4.ds-py-3>a");
+    let cardLinksArr = $(".ds-px-4.ds-py-3>a");
+    let scoreCardLinkArr=[];
+    for(let i=0; i<cardLinksArr.length; i++)
+    {
+        let scoreCardLink = cardLinksArr[i].attribs.href;
+        scoreCardLink = "https://www.espncricinfo.com"+ scoreCardLink;
+        scoreCardLinkArr.push(scoreCardLink);
+    }
+    console.log(scoreCardLinkArr);
     // console.log(cardLinksArr);
-    // console.log(cardLinksArr.length);
-    // let scoreCardLinks=[];
-    // for(let i=0; i<cardLinksArr.length; i++)
-    // {
-    //     let tempLink = cardLinksArr[i].attribs["href"];
-    //     if(tempLink.includes("full-scorecard"))
-    //     console.log(tempLink);
-    // }
-    // console.log("`````````````````````````````````");
-    // console.log(scoreCardLinks);
-    
 }
